7ded5294b51d9a2162fcca4af9714f2b877c9122
Harrison Plume <Harrison@pwr.local>
2021-07-09 09:22:43 +1000

    Update 0.2.2

--- catalog/urls.py
+++ catalog/urls.py
@@ -5,34 +5,18 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("CPTasks/", views.CpTaskListView.as_view(), name="cptasks"),
     path("CPTask/<int:pk>", views.CPTaskDetailView.as_view(), name = "cptask-detail"),
+    path("CPTask/<int:pk>/updatestatus", views.CPTaskStatusUpdate.as_view(), name = "cptask-update-status"),
     ]
-
-urlpatterns += [
-    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-]
-
-urlpatterns += [
-    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
-]
 
 urlpatterns += [
     path('book/renew/success/', views.BookRenewSuccess, name='bookrenewsuccess'),
 ]
 
-urlpatterns += [
-    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
-    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
-    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
-]
-
-urlpatterns += [
-    path('book/create/', views.BookCreate.as_view(), name='book-create'),
-    path('book/<int:pk>/update/', views.BookUpdate.as_view(), name='book-update'),
-]
 
 urlpatterns += [
     path("parts/", views.PartListView.as_view(), name = "parts"),
     path("part/<int:pk>", views.PartDetailView.as_view(), name = "part-detail"),
     path('part/create/', views.PartCreate.as_view(), name='part-create'),
     path('part/<int:pk>/update/', views.PartUpdate.as_view(), name='part-update'),
+    path('part/<int:pk>/delete/', views.PartDelete.as_view(), name='part-delete'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -17,22 +17,17 @@ def index(request):
     #Generate counts for soe of the main objetcs
     num_CpTasks = ComponentPrepTaskInstance.objects.all().count()
     
-    #Avivable books (status = "a")
-    num_instances_avaliable = BookInstance.objects.filter(status__exact="a").count()
+    #avalible tasks remaining
+    tasks_remaning = ComponentPrepTaskInstance.objects.exclude(status__exact="c").count()
     
     #all() is implied by default
-    num_authors = Author.objects.count()
+    num_parts = Part.objects.all().count()
     
-    #Number of site visits by the current user
-    num_visits = request.session.get("num_visits", 0)
-    request.session["num_visits"] = num_visits + 1
-    
-    
+    #Number of site visits by the current user    
     context = {
         "num_CpTasks": num_CpTasks,
-        "num_instances_available": num_instances_avaliable,
-        "num_authors": num_authors,        
-        "num_visits": num_visits,
+        "num_parts": num_parts,  
+        "tasks_remaning":tasks_remaning,
         }
     
     #Render the HTML template index.html with the data in the context vairable
@@ -54,6 +49,18 @@ class CpTaskListView(generic.ListView):
     context_object_name = "cptask_list"
     template_name = "cptask_list.html"
     paginate_by = 30
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        num_tasks_not_started = ComponentPrepTaskInstance.objects.filter(status__exact="a").count()
+        tasks_remaining = ComponentPrepTaskInstance.objects.exclude(status__exact="c").count()
+        tasks_complete = ComponentPrepTaskInstance.objects.filter(status__exact="c").count()
+        tasks_on_hold = ComponentPrepTaskInstance.objects.filter(status__exact="h").count()
+        context["num_tasks_not_started"] = num_tasks_not_started
+        context["tasks_remaining"] = tasks_remaining
+        context["tasks_complete"] = tasks_complete
+        context["tasks_on_hold"] = tasks_on_hold
+        return context
  
 
 class CPTaskDetailView(generic.DetailView):
@@ -69,57 +76,10 @@ class PartListView(generic.ListView):
 class PartDetailView(generic.DetailView):
     model = Part
     
-class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
-    """Generic class-based view listing books on loan to current user."""
-    model = BookInstance
-    template_name = "catalog/bookinstance_list_borrowed_user.html"
-    paginate_by = 10
-    
-    def get_queryset(self):
-        return BookInstance.objects.filter(borrower = self.request.user).filter(status__exact="o").order_by("due_back")
     
 import datetime
 
 from django.contrib.auth.decorators import login_required, permission_required
-
-
-@login_required
-@permission_required('catalog.can_mark_returned', raise_exception=True)
-def renew_book_librarian(request, pk):
-    """View function for renewing a specific BookInstance by librarian."""
-    book_instance = get_object_or_404(BookInstance, pk=pk)
-
-    # If this is a POST request then process the Form data
-    if request.method == 'POST':
-
-        # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
-
-        # Check if the form is valid:
-        if form.is_valid():
-            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data['renewal_date']
-            book_instance.save()
-
-            # redirect to a new URL:
-            return HttpResponseRedirect(reverse('bookrenewsuccess'))
-
-    # If this is a GET (or any other method) create the default form.
-    else:
-        proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
-
-    context = {
-        'form': form,
-        'book_instance': book_instance,
-    }
-
-    return render(request, 'catalog/book_renew_librarian.html', context)
-
-class AuthorCreate(LoginRequiredMixin,CreateView):
-    model = Author
-    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
-    initial = {'date_of_death': '11/06/2020'}
     
 class PartCreate(LoginRequiredMixin,CreateView):
     model = Part 
@@ -132,18 +92,11 @@ class PartUpdate(LoginRequiredMixin,UpdateView):
     fields = '__all__'
     success_url = reverse_lazy('parts')
     
-class AuthorUpdate(LoginRequiredMixin,UpdateView):
-    model = Author
-    fields = '__all__' 
-
-class AuthorDelete(LoginRequiredMixin,DeleteView):
-    model = Author
-    success_url = reverse_lazy('authors')
+class PartDelete(LoginRequiredMixin,DeleteView):
+    model = Part 
+    success_url = reverse_lazy('parts')
     
-class BookCreate(LoginRequiredMixin,CreateView):
-    model = Book
-    fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
 
-class BookUpdate(LoginRequiredMixin,UpdateView):
-    model = Book
-    fields = '__all__' # Not recommended (potential security issue if more fields added)
+class CPTaskStatusUpdate(LoginRequiredMixin,UpdateView):
+    model = ComponentPrepTaskInstance
+    fields = ['status'] # Not recommended (potential security issue if more fields added)
