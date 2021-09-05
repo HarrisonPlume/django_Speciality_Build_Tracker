44cd1818ef782cb22c456bfab9e4263143952081
Harrison Plume <Harrison@pwr.local>
2021-09-06 07:30:41 +1000

    Version 1.0.1 Added Bat files

---
+++ RunServer.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+"D:\Django\venv\Scripts\python.exe" "D:\Django\django_Speciality_Build_Tracker\manage.py" "runserver"
+start C:\"Program Files (x86)"\Google\Chrome\Application\chrome.exe "http://127.0.0.1:8000/"
+pause
---
+++ UploadToGit.bat
@@ -0,0 +1,5 @@
+@echo off
+git add -A
+git commit -m "Version 1.0.1 Added Bat files"
+git push origin main
+pause
